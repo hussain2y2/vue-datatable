425243f49af545c7054af3492380db4236ffcc6a
Hussain Ahmad <engrhussainahmad@gmail.com>
2019-11-07 18:52:21 +0500

    Static data added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,25 +6,19 @@ All notable changes to this package will be documented in this file.
 
 ### Added
 
-- Static Page Length
-- Static Search
-- Static Pagination
-- Entries Status
-
-## [0.1.6 - 07-11-2019](https://www.npmjs.com/package/server-datatable/v/0.1.6)
-
-### Added
-
-- Style
-
-## [0.1.7 - 07-11-2019](https://www.npmjs.com/package/server-datatable/v/0.1.7)
-
-## Changes
-
-- CHANGELOG version linked.
+- Static Page Length.
+- Static Search.
+- Static Pagination.
+- Entries Status.
 
 ## [0.1.8 - 07-11-2019](https://www.npmjs.com/package/server-datatable/v/0.1.8)
 
 ## Changes
 
 - src and public directories included.
+
+## [0.1.9 - 07-11-2019](https://www.npmjs.com/package/server-datatable/v/0.1.9)
+
+## Added
+
+- Static table dummy data.
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -21,7 +21,121 @@
         </div>
       </div>
     </div>
-    <div class="row"></div>
+    <div class="row">
+      <table class="table table-striped table-bordered dataTable" style="width: 100%;">
+        <thead>
+        <tr role="row">
+          <th class="sorting_asc" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name" >Name</th>
+          <th class="sorting" rowspan="1" colspan="1" aria-label="Position">Position</th>
+          <th class="sorting" rowspan="1" colspan="1" aria-label="Office">Office</th>
+          <th class="sorting" rowspan="1" colspan="1" aria-label="Age">Age</th>
+          <th class="sorting" rowspan="1" colspan="1" aria-label="Start Date">Start Date</th>
+          <th class="sorting" rowspan="1" colspan="1" aria-label="Salary">Salary</th>
+<!--          <th class="sorting_asc_disabled sorting_desc_disabled">Actions</th>-->
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Tiger Nixon</td>
+          <td>System Architect</td>
+          <td>Edinburgh</td>
+          <td>61</td>
+          <td>2011/04/25</td>
+          <td>$320,800</td>
+        </tr>
+        <tr>
+          <td>Garrett Winters</td>
+          <td>Accountant</td>
+          <td>Tokyo</td>
+          <td>63</td>
+          <td>2011/07/25</td>
+          <td>$170,750</td>
+        </tr>
+        <tr>
+          <td>Ashton Cox</td>
+          <td>Junior Technical Author</td>
+          <td>San Francisco</td>
+          <td>66</td>
+          <td>2009/01/12</td>
+          <td>$86,000</td>
+        </tr>
+        <tr>
+          <td>Cedric Kelly</td>
+          <td>Senior Javascript Developer</td>
+          <td>Edinburgh</td>
+          <td>22</td>
+          <td>2012/03/29</td>
+          <td>$433,060</td>
+        </tr>
+        <tr>
+          <td>Airi Satou</td>
+          <td>Accountant</td>
+          <td>Tokyo</td>
+          <td>33</td>
+          <td>2008/11/28</td>
+          <td>$162,700</td>
+        </tr>
+        <tr>
+          <td>Brielle Williamson</td>
+          <td>Integration Specialist</td>
+          <td>New York</td>
+          <td>61</td>
+          <td>2012/12/02</td>
+          <td>$372,000</td>
+        </tr>
+        <tr>
+          <td>Herrod Chandler</td>
+          <td>Sales Assistant</td>
+          <td>San Francisco</td>
+          <td>59</td>
+          <td>2012/08/06</td>
+          <td>$137,500</td>
+        </tr>
+        <tr>
+          <td>Rhona Davidson</td>
+          <td>Integration Specialist</td>
+          <td>Tokyo</td>
+          <td>55</td>
+          <td>2010/10/14</td>
+          <td>$327,900</td>
+        </tr>
+        <tr>
+          <td>Colleen Hurst</td>
+          <td>Javascript Developer</td>
+          <td>San Francisco</td>
+          <td>39</td>
+          <td>2009/09/15</td>
+          <td>$205,500</td>
+        </tr>
+        <tr>
+          <td>Sonya Frost</td>
+          <td>Software Engineer</td>
+          <td>Edinburgh</td>
+          <td>23</td>
+          <td>2008/12/13</td>
+          <td>$103,600</td>
+        </tr>
+        <tr>
+          <td>Jena Gaines</td>
+          <td>Office Manager</td>
+          <td>London</td>
+          <td>30</td>
+          <td>2008/12/19</td>
+          <td>$90,560</td>
+        </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+          <th>Name</th>
+          <th>Position</th>
+          <th>Office</th>
+          <th>Age</th>
+          <th>Start date</th>
+          <th>Salary</th>
+        </tr>
+        </tfoot>
+      </table>
+    </div>
     <div class="row">
       <div class="col-sm-12 col-md-5">
         <div class="dataTables_info" role="status">
